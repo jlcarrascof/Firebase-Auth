c4b693457b837ae32c746b655ccf41baaa820a0a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-24 11:23:37 -0400

    Importing the required environment variables

--- firebaseAuth/src/App.jsx
+++ firebaseAuth/src/App.jsx
@@ -2,6 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+// Importing the required environment variables
+const apiKey = process.env.REACT_APP_API_KEY;
+const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
+const projectId = process.env.REACT_APP_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_APP_ID;
 
 function App() {
 
