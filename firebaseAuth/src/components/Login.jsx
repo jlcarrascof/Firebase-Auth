8677627d5d5ffc5f8cc16a736ed6b539321df65e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-27 10:46:19 -0400

    Setup Login.jsx

--- firebaseAuth/src/components/Login.jsx
+++ firebaseAuth/src/components/Login.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Image from '../assets/OIP.jpeg'
 
 const Login = () => {
   return (
-    <div>Login Component</div>
+    <div className='container'>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="father">
+            <div className='card card-body'>
+              <form>
+                <input type="email" placeholder="Enter Email" className='form-control' />
+              </form> 
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <img src={Image} alt="OIP" className='imageSize' />
+        </div>
+      </div>
+    </div>
   )
 }
 
